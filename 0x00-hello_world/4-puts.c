a7de0bf486d0d725287fa713a1d8ad5fdbb757fd
MICENDALE <michaelendale16@gmail.com>
2022-07-01 09:14:10 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,16 +8,12 @@
  *
  *   *Return: Always 0 (Success)
  *
- *    */
-
-
+ *
+     */
 
 int main(void)
 
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 			return (0);
-
 }
